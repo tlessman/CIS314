a445745cd08b5a896f09a31d028ad06d644c77ab
Theodore Lessman <tlessman@uoregon.edu>
2017-01-16 23:55:56 -0800

    completed 2-60, debug intact

--- 2-60.c
+++ 2-60.c
@@ -2,53 +2,60 @@
 
 
 void pAND (int a, int m){
-	printf("0x%x\n", a);
-	printf("0x%x\n", m);
+	printf("0x%.8x\n", a);
+	printf("0x%.8x\n", m);
 	printf("& ________\n");
-	printf("0x%x\n\n", a&m);
+	printf("0x%.8x\n\n", a&m);
 }
 
 
 void pOR (int a, int m){
-	printf("0x%x\n", a);
-	printf("0x%x\n", m);
+	printf("0x%.8x\n", a);
+	printf("0x%.8x\n", m);
 	printf("| ________\n");
-	printf("0x%x\n\n", a|m);
+	printf("0x%.8x\n\n", a|m);
 }
 
 void pXOR (int a, int m){
-	printf("0x%x\n", a);
-	printf("0x%x\n", m);
+	printf("0x%.8x\n", a);
+	printf("0x%.8x\n", m);
 	printf("^ ________\n");
-	printf("0x%x\n\n", a^m);
+	printf("0x%.8x\n\n", a^m);
 }
 
 void pNOT (int a){
-	printf("0x%x\n", a);
+	printf("0x%.8x\n", a);
 	printf("~ ________\n");
-	printf("0x%x\n\n", ~a);
+	printf("0x%.8x\n\n", ~a);
 }
 
-unsigned int replace_byte(unsigned x, int i, unsigned short b){
+void pLS (int a, int i){
+	printf("0x%.8x\n", a);
+	printf("<<_______%i\n", i);
+	printf("0x%.8x\n\n", a<<8*i);
+}
+
+unsigned int replace_byte(unsigned x, int i, unsigned char b){
 	int n = 0x000000FF;
 	int w = 8;
+	unsigned int m = b;
                                     // (i, 2, b)
 	printf("N is 0x%.8x\n", n); // N is 0x000000FF
 	
-	printf("B is 0x%.8x\n", b); // B is 0x000000AB
+	printf("M is 0x%.8x\n", m); // m is 0x000000AB
 
 	printf("X is 0x%.8x\n", x); // X is 0x12345678
 
 	n = n << w*i;               // n = 0x000000FF << 16
 	printf("N to 0x%.8x\n", n); // N is 0x00FF0000
 	
-	b = b << w*i;               // b = 0x000000AB << 16
-        printf("B is 0x%.8x\n", b); // b = 0x00AB0000 (getting 0x00000000)
+	m = m << w*i;               // m = 0x000000AB << 16
+        printf("M is 0x%.8x\n", m); // m = 0x00AB0000 (getting 0x00000000)
 
 	x &= ~n;                    // x = 0x12345678 & 0xFF00FFFF
 	printf("X is 0x%.8x\n", x); // X is 0x12005678
 	 	
-	return x^b; //rtrn 0x12AB5678 (getting 0x12005678)
+	return x^m; //rtrn 0x12AB5678 (getting 0x12005678)
 	
 }
 
@@ -62,7 +69,7 @@ unsigned int replace_byte(unsigned x, int i, unsigned short b){
 
 int main(){
 	unsigned int a = 0x12345678;
-        unsigned short b = 0xAB;
+        unsigned char b = 0xAB;
 	
 /*
 	pAND(a,m<<8);
@@ -76,9 +83,16 @@ int main(){
 */
 	printf("rtrn 0x%x\n\n", replace_byte(a, 2, b));
 	printf("rtrn 0x%x\n\n", replace_byte(a, 0, b));
+	printf("rtrn 0x%x\n\n", replace_byte(a, 1, b));
+	printf("rtrn 0x%x\n\n", replace_byte(a, 3, b));
+
 	unsigned char f = 0xAB;
 	unsigned short g = 0x000000AB;
-	printf("char 0xAB == short 0x000000AB ? %i", f==g); 
+	//printf("char 0xAB == short 0x000000AB ? %i", f==g); 
+
+	unsigned int r = 2;
+	unsigned int s = 0x000000AB;
+	pLS(s, r);
 
 	return 0;
 }
