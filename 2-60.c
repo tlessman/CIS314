c6024df7d19e703d77aa51e70a29a9cfaed58ba3
Theodore Lessman <tlessman@uoregon.edu>
2017-01-17 00:17:21 -0800

    cleaned up 2-60

--- 2-60.c
+++ 2-60.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
-
+/* DEBUG FUNCTIONS
 void pAND (int a, int m){
 	printf("0x%.8x\n", a);
 	printf("0x%.8x\n", m);
 	printf("& ________\n");
 	printf("0x%.8x\n\n", a&m);
 }
-
 
 void pOR (int a, int m){
 	printf("0x%.8x\n", a);
@@ -35,64 +34,43 @@ void pLS (int a, int i){
 	printf("0x%.8x\n\n", a<<8*i);
 }
 
+void pRS (int a, int i){
+        printf("0x%.8x\n", a);
+        printf(">>_______%i\n", i);
+        printf("0x%.8x\n\n", a>>8*i);
+}
+*/
+
 unsigned int replace_byte(unsigned x, int i, unsigned char b){
 	int n = 0x000000FF;
 	int w = 8;
 	unsigned int m = b;
-                                    // (i, 2, b)
-	printf("N is 0x%.8x\n", n); // N is 0x000000FF
-	
-	printf("M is 0x%.8x\n", m); // m is 0x000000AB
-
-	printf("X is 0x%.8x\n", x); // X is 0x12345678
-
+	printf("replace_byte(0x%.8x, %i, 0x%.2x)\n", x, i, b);
+ 	// replace_byte(i, 2, b)
+	// N is 0x000000FF
+	// m is 0x000000AB
+	// X is 0x12345678
 	n = n << w*i;               // n = 0x000000FF << 16
-	printf("N to 0x%.8x\n", n); // N is 0x00FF0000
-	
+	printf("0x%.8x\n", n); // N is 0x00FF0000
 	m = m << w*i;               // m = 0x000000AB << 16
-        printf("M is 0x%.8x\n", m); // m = 0x00AB0000 (getting 0x00000000)
-
+        printf("0x%.8x\n", m); // m is 0x00AB0000
 	x &= ~n;                    // x = 0x12345678 & 0xFF00FFFF
-	printf("X is 0x%.8x\n", x); // X is 0x12005678
+	printf("0x%.8x\n", x); // X is 0x12005678
 	 	
-	return x^m; //rtrn 0x12AB5678 (getting 0x12005678)
+	return x^m;                 //rtrn 0x12AB5678
 	
 }
-
-
-
-
-
-
-
-
 
 int main(){
 	unsigned int a = 0x12345678;
         unsigned char b = 0xAB;
-	
-/*
-	pAND(a,m<<8);
-	pOR(a,m<<8);
-	pXOR(a,m<<8);
-	pNOT(a);
-	pAND(a,n<<8);
-	pOR(a,n<<8);
-	pXOR(a,n<<8);
-	pNOT(n);
-*/
-	printf("rtrn 0x%x\n\n", replace_byte(a, 2, b));
-	printf("rtrn 0x%x\n\n", replace_byte(a, 0, b));
-	printf("rtrn 0x%x\n\n", replace_byte(a, 1, b));
-	printf("rtrn 0x%x\n\n", replace_byte(a, 3, b));
 
-	unsigned char f = 0xAB;
-	unsigned short g = 0x000000AB;
-	//printf("char 0xAB == short 0x000000AB ? %i", f==g); 
+	printf("0x%x\n\n", replace_byte(a, 2, b));
+	printf("0x%x\n\n", replace_byte(a, 0, b));
+	printf("0x%x\n\n", replace_byte(a, 1, b));
+	printf("0x%x\n\n", replace_byte(a, 3, b));
 
-	unsigned int r = 2;
-	unsigned int s = 0x000000AB;
-	pLS(s, r);
+	//char 0xAB == int 0x000000AB 
 
 	return 0;
 }
