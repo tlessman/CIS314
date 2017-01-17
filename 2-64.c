365d1ebf351713a5a3c4d26c3d77a61e02bc08fe
Theodore Lessman <tlessman@uoregon.edu>
2017-01-17 12:07:42 -0800

    initial commit for 2-64.c. commented cases.

---
+++ 2-64.c
@@ -0,0 +1,24 @@
+/* 2-64.c
+ * return 1 when any odd bit of x equals 1; 0 otherwise. assume w=32
+ * follow bit-level integer coding
+ * 0x00000000
+ * s 12345678
+ *
+ * 0x12345678
+ * > 1
+ *
+ * 0xFFFFFFFF
+ * >        0
+ *
+ * 0xF1F1F1F1
+ * >        0
+ * 
+ * 0x218E1D1A
+ * >     1 
+ *
+ *
+ *
+ *
+ *
+ *
+ * /
