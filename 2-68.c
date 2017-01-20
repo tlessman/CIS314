ea2d5c1d390c22437bfa1c26dab4250fa34c1ae0
Theodore Lessman <tlessman@uoregon.edu>
2017-01-19 20:50:08 -0800

    added and finished prototype of 2-68.c

---
+++ 2-68.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+mask least significant bits
+
+int lower_one_mask(int n){
+	
+}
+
+int main(){
+	unsigned int a = 7;
+
+	/*DEBUG CODE*/
+	unsigned int m = (FFFFFFFF>>a);
+	printf("0x%.8x\n", a|m);
+
+
+
+
+
+
+	//printf("0x.8%x \n", lower_one_mask(a));
+	return 0;
+}
