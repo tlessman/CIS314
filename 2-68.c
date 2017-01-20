cd7fdefa81fff58b7de0b3287d1117e49fdfee64
Theodore Lessman <tlessman@uoregon.edu>
2017-01-20 08:57:08 -0800

    brainstorming 6-68.c

--- 2-68.c
+++ 2-68.c
@@ -1,19 +1,36 @@
 #include <stdio.h>
 
 //mask least significant bits
+//
+// 0x00000001 << 5
+// 0x00010000 
+//
+// 0xFFFFFFFF >> 4
+// 0x0000FFFF 
+//
+// 0x00010000
+//&0x0000FFFF
+// 0x0001FFFF 
+//
+//
 
 int lower_one_mask(int x){
-	unsigned w = 32;
-	unsigned int m = (0xFFFFFFFF<<(w-1));
-        return (x^m)>>(w-1);
+	unsigned w = 8;
+	unsigned a = 1;
+	unsigned m = 0xFFFFFFFF;
+	a<x;            //0x00010000
+	m>(w-x);         //0x0000FFFF
+	
+	return (a^m);
+	
 
 }
 
 int main(){ 
-	unsigned int i = 7;
-//	for (i = 0; i<32; i++){
-		printf("%i: %i\n", i+1,lower_one_mask(i+1));
-//	}
+	unsigned i;
+	for (i = 0; i<5 ; i++){
+		printf("\n%i: %i\n0x%.8x\n", i+1, lower_one_mask(i+1), lower_one_mask(i+1));
+	}
 
 
 
