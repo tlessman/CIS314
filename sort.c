03661d43ec7aef892e5b63c81aa6c09144818351
Theodore <tlessman@uoregon.edu>
2017-02-01 12:16:53 -0800

    mod print function

--- sort.c
+++ sort.c
@@ -5,17 +5,25 @@ void swap(int* xp, int* yp);
 
 void printArray(int* arr, int s);
 
+int* selSortArray(int* arr, int s);
+
 int main(){
 	int a_size;
 	printf("Enter size of array: ");
 	scanf("%d", &a_size);
+	
 	int* array = malloc(a_size * sizeof(int));
+	
 	int i;
 	for (i = 0; i < a_size; i++){
 		printf("Enter value for array[%d]: ", i);
 		scanf("%d", array+i);
 	}
+	
+	printf("Your array before swapping: \n");
 	printArray(array, a_size);
+
+	
 	
 	return 0;
 }
@@ -32,4 +40,12 @@ void printArray(int* arr, int s){
 	for (i = 0; i < s; i++){
 		printf("%d:[%d]\n", i, *arr+i);
 	}
+}
+
+void selSortArray(int* arr, int s){
+	int i;
+	for (){
+		
+	}
+	
 }
