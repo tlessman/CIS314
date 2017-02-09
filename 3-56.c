274c37fc926a8d0726d5bd67c62c50c5d3236b46
Theodore <tlessman@uoregon.edu>
2017-02-08 16:30:10 -0800

    removed old 3.56, initial and commented 3-56.c

---
+++ 3-56.c
@@ -0,0 +1,26 @@
+/** x@%ebp+8 (passed int), n@%ebp+12 (loop index)
+ * movl 8(%ebp), %esi    //move x from parameter to esi
+ * movl 12(%ebp), %ebx   //move n from parameter to ebx
+ * movl $-1, %edi        //edi = -1 (0xFFFFFFFF)
+ * movl $1, %edx         //edx = 1 (0x00000001)
+ * .L2:                  //begin for
+ * movl %edx, %eax       //eax = 1
+ * andl %esi, %eax       //x&1 into eax
+ * xorl %eax, %edi       //eax^-1 into edi
+ * movl %ebx, %ecx       //ecx = n //loop index
+ * sall %cl, %edx        //shift left by cl //low(ecx)
+ * testl %edx, %edx      //check if zero'd out
+ * jne .L2               //if != 0, goto L2 else:
+ * movl %edi, %eax       //return result
+**/
+
+int loop(int x, int n){
+	int result = /**/;
+	int mask;
+	for (mask = -1; mask/**/; mask = /**/){
+		result ^= /**/;
+	}
+	return result;
+}
+
+
