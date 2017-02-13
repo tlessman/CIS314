0b281bff612606feb5739a5b3a1c7aed9d2d2614
Theodore <tlessman@uoregon.edu>
2017-02-13 11:56:14 -0800

    updated 3-56.c - !-O1 has additional L3:, registers are neg; -O1 has almost no similarity to provided code

--- TheodoreLessmanAssignment4/3-56.c
+++ TheodoreLessmanAssignment4/3-56.c
@@ -1,12 +1,12 @@
 /** x@%ebp+8 (passed int), n@%ebp+12 (loop index)
- * movl 8(%ebp), %esi    //move x from parameter to esi
- * movl 12(%ebp), %ebx   //move n from parameter to ebx
- * movl $-1, %edi        //edi = -1 (0xFFFFFFFF)
- * movl $1, %edx         //edx = 1 (0x00000001)
- * .L2:                  //begin for
- * movl %edx, %eax       //eax = 1
- * andl %esi, %eax       //x&1 into eax
- * xorl %eax, %edi       //eax^-1 into edi
+ * movl 8(%ebp), %esi    // esi = x
+ * movl 12(%ebp), %ebx   // ebx = n
+ * movl $-1, %edi        // edi = -1 (0xFFFFFFFF)
+ * movl $1, %edx         // edx = 1 (0x00000001)
+ * .L2:                  // begin for
+ * movl %edx, %eax       // eax = 1
+ * andl %esi, %eax       // x&1 into eax
+ * xorl %eax, %edi       // eax^-1 into edi
  * movl %ebx, %ecx       //ecx = n //loop index
  * sall %cl, %edx        //shift left by cl //low(ecx)
  * testl %edx, %edx      //check if zero'd out
@@ -17,18 +17,17 @@
 /**
  * A. x in esi, n in ebx, result in eax, mask in edi
  * B. result = 1, mask = -1
- * C. 
- * D. 
- * E. 
- * F. 
+ * C. mask != 0 
+ * D. as an index
+ * E. within the loop
  *
 **/
 
 int loop(int x, int n){
 	int result = 1;
 	int mask;
-	for (mask = -1; mask""""""; mask = """"""){
-		result ^= """""";
+	for (mask = -1; mask != 0; mask = mask + 1){
+		result ^= mask;
 	}
 	return result;
 }
