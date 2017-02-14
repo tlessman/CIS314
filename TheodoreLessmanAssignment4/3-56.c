44bdd559824e61c1d2e75f0d041b2f102e736ec0
Theodore <tlessman@uoregon.edu>
2017-02-13 21:57:03 -0800

    attempting to get 3-62 to transpose

--- TheodoreLessmanAssignment4/3-56.c
+++ TheodoreLessmanAssignment4/3-56.c
@@ -27,7 +27,7 @@
 int loop(int x, int n){
 	int result = -1;
 	int mask;
-	for (mask = 1; mask!=0; (mask = (mask << n))){
+	for (mask = 1; mask!=0; mask = mask << (n&0xFF)){
 		result ^= (mask & x);
 	}
 	return result;
--- TheodoreLessmanAssignment4/3-62.c
+++ TheodoreLessmanAssignment4/3-62.c
@@ -1,18 +1,13 @@
-#include <stdio.h>
-
 /**
  * A. M = 13 //bytes in row / bytes in int = 52 / 4
  * j in esi, i in ecx
 **/
 
-#define M 13
-typedef int Marray_t[M][M]; 
-
 /**
  * .L3:
- * 	movl (%ebx), %eax            //move eax = mArr    (mArr)
- * 	movl (%esi, %ecx, 4), %edx   //edx = esi + 4ecx   eax = a[i + 4j] 
- * 	movl %eax, (%esi, %ecx, 4)   //esi + 4ecx = eax   a[i + 4j] = eax 
+ * 	movl (%ebx), %eax            //eax = A  (mArr)
+ * 	movl (%esi, %ecx, 4), %edx   //load *((ecx * 4) + esi) into edx     temp = A[i][j]
+ * 	movl %eax, (%esi, %ecx, 4)   //load edx into *))ecx * 4) + esi)     
  * 	addl $1, %ecx                //ecx++              j++
  * 	movl %edx, (%ebx)            //(ebx) = edx        *a = j + 4i
  * 	addl $52, (%ebx)             //(ebx) += 52        
@@ -25,30 +20,54 @@ A[i][j]
 *(A + i)[j] || *(A[i] + j)
 *((*(A + i)) + j)
 **/
-#define void print_Marray(Marray_t A){
+
+#include <stdio.h>
+
+#define M 13
+typedef int Marray_t[M][M]; 
+
+void pop_array(Marray_t A){
+	
+	int i, j;	
+	for (j = 0; j < M; j++){
+		for (i = 0; i < M; i++){
+			A[i][j] = j;
+		}	
+	}
+}
+
+void print_array(Marray_t A){
 	int i, j;
-	for (i = 0; i < M; i++){
-		for (j = 0; j < M; j++){
-			printf("%8d   ", *((*(A + i)) + j) );
-
-
-
-#define void opt_transpose(Marray_t A){
-	int i, j;
-	int *p = A;
-	for (i = 0; i < M; i++){
-		int* p1 = p + (i*M+j);
-		int* p2 = p + (j*M+i);
-			int t1 = *p1;
-			*p1 = *p2;
-			*p2 = t1;
+	for (j = 0; j < M; j++){
+		for (i = 0; i < M; i++){
+			printf("[%4d]", A[i][j]);
 		}
+		printf("\n");
+	}
+
+	printf("\n");	
+}
+
+
+void opt_transpose(Marray_t A){
+	int i, j;
+	int* t1;
+	int* t2;	
+	for (j = 0; j < i; j++){
+		//*((*A+i)+j);
+		t1 = (*(A+i) + j);
+		*(*(A+i) + j) = *(*(A+j) + i);
+		*(*(A+j) + i) = *t1;
+		j + (4*M);
 	}
 }
 
 int main(){
-	Marray_t A = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}}
-	opt_transpose(mArr);	
 
+	Marray_t A;
+	pop_array(A);
+	print_array(A);
+	opt_transpose(A);	
+	print_array(A);
 	return 0;
 }
