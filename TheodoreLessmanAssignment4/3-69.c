20726fbec49071597f88197a2c7d1c265fe91ddb
Theodore <tlessman@uoregon.edu>
2017-02-13 23:59:52 -0800

    rdy to sub

--- TheodoreLessmanAssignment4/3-69.c
+++ TheodoreLessmanAssignment4/3-69.c
@@ -11,12 +11,12 @@
  * long trace(tree_ptr tp);
  *
  * trace:                     //tp in %rdi
- *	movl $0, %eax         //:
- *	testq %rdi, $rdi
- *	je .L3
+ *	movl $0, %eax         //eax = 0 (not sure why? NULL maybe?)
+ *	testq %rdi, $rdi      //if tp = 0 (if root is null)
+ *	je .L3                //goto L3 (break)
  * .L5:
- *	movq (%rdi), %rax
- *	movq 16(%rdi), %rdi
+ *	movq (%rdi), %rax     //rax = &tp
+ *	movq 16(%rdi), %rdi   //rdi = &tp. 
  *	testq %rdi, %rdi
  *	jne .L5
  * .L3:
@@ -29,4 +29,24 @@
  * B. 
 **/
 
+typedef struct ELE *tree_ptr;
+struct ELE{
+	long val;
+	tree_ptr left;
+	tree_ptr right;
+}
 
+long trace(tree_ptr tp){
+	int v = NULL;
+	while(tp.value!= 0){
+		if (tp.value == v){
+			break;
+		}
+		trace(this->tree_ptr.left);
+		trace(this->tree_ptr.right);
+}
+
+int main(){
+
+	return 0;
+}
